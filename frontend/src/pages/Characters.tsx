a5b21d2a50564975690fed2eab916f57d9a435ff
jaystas <jays.staigers@gmail.com>
2026-01-24 05:43:04 -0600

    column sizes

--- frontend/src/pages/Characters.tsx
+++ frontend/src/pages/Characters.tsx
@@ -3,7 +3,7 @@ import CharacterEditor from '@/components/characters/CharacterEditor'
 function CharactersPage() {
   return (
     <div className="h-full w-full p-6">
-      <div className="grid h-full w-full gap-8 lg:grid-cols-[1fr_3fr]">
+      <div className="grid h-full w-full gap-8 lg:grid-cols-[1.25fr_3fr]">
         <div className="h-full w-full" />
         <div className="h-full w-full">
           <CharacterEditor />
